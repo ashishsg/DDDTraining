c8cf3796ef0515685e9389be9cef1a94c4b8386f
ashishsg <ashish.sgokhale@gmail.com>
2022-11-04 22:53:18 +0530

    project setup

--- src/main/java/ddd/training/Main.java
+++ src/main/java/ddd/training/Main.java
@@ -2,6 +2,6 @@ package ddd.training;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world Test!");
     }
 }
